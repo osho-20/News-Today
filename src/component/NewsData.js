e27525bbc31a8932e16778926fcf2173328ff923
YASH JAIN <yashjainiiita@gmail.com>
2023-03-10 13:52:09 +0530

    start

---
+++ src/component/NewsData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsData = () => {
+    return (
+        <div className='main'>
+            <h1>News Data</h1>
+        </div>
+    )
+}
+
+export default NewsData;
